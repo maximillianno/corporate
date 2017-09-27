505219c74069aec297f647f7a5699d15e8cee719
Maxim S <maxus.star@gmail.com>
2017-09-27 18:24:19 +0400

    родительский и индекс контроллеры

---
+++ resources/views/pink/layouts/site.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maxus
+ * Date: 22.09.2017
+ * Time: 19:10
+ */
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('/', 'IndexController', [
+            'only' => ['index'],
+            'names' => ['index'  => 'home']
+
+]);
